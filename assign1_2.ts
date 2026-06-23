f569f953d2b2dcd80cce4fc5d66f7c9ec44d128b
Jirawat <highdiffbot2@gmail.com>
2026-06-23 11:07:00 +0700

    ส่งงาน Assignment1_2

--- assign1_2.ts
+++ assign1_2.ts
@@ -3,10 +3,9 @@ let isbn: string = "978-616-000-000-1";
 let price: number = 650;
 let isAvailable: boolean = true;
 let bookSummary: [string, number] = [bookTitle, price];
-let discountedPrice: number = price - (price * 0.15);
-let isPremium: boolean = price > 500;
-let canDisplayInShowcase: boolean = isPremium && isAvailable;
-let displayStatus: string = (canDisplayInShowcase && "สามารถจัดแสดงที่หน้าตู้โชว์ได้") || "ไม่สามารถจัดแสดงที่หน้าตู้โชว์ได้";
+let newPrice: number = price - (price * 0.15);
+let premium: boolean = price > 500;
+let showBook: boolean = premium && isAvailable;
 
-console.log(`ชื่อหนังสือ: ${bookSummary[0]} \nISBN: ${isbn} \nราคาปก: ${bookSummary[1]} บาท \nราคาหลังลด 15%: ${discountedPrice} บาท \nเป็นหนังสือ Premium: ${isPremium} \nสถานะว่าง: ${isAvailable} \nสถานะการจัดแสดง: ${displayStatus}`);
+console.log(`ชื่อหนังสือ: ${bookSummary[0]} \nISBN: ${isbn} \nราคาปก: ${bookSummary[1]} บาท \nราคาหลังลด 15%: ${newPrice} บาท \nเป็นหนังสือ Premium: ${premium} \nสถานะว่าง: ${isAvailable} \nสามารถจัดแสดงหน้าตู้ได้: ${showBook}`);
                                                     
