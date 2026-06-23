66f6635ce73b53d150222af0e48609d49cb53a3f
Jirawat <highdiffbot2@gmail.com>
2026-06-23 11:06:33 +0700

    ส่งงาน Assignment1_1

--- assign1_1.ts
+++ assign1_1.ts
@@ -4,9 +4,8 @@ let quantity: number = 2;
 let isMember: boolean = false;
 
 let totalPrice: number = pricePerUnit * quantity;
-let hasDiscount: boolean = totalPrice > 1000 || isMember;
-let discountRate: number = (hasDiscount && 0.10) || 0;
-let discountAmount: number = totalPrice * discountRate;
-let finalPrice: number = totalPrice - discountAmount;
+let discount: boolean = totalPrice > 1000 || isMember;
+let discountPrice: number = totalPrice * 0.10 * Number(discount);
+let finalPrice: number = totalPrice - discountPrice;
 
-console.log(`ชื่อสินค้า: ${productName} \nจำนวนที่ซื้อ: ${quantity} ชิ้น \nราคารวม: ${totalPrice} บาท \nได้รับส่วนลด: ${hasDiscount} \nราคาที่ต้องจ่าย: ${finalPrice} บาท`);
+console.log(`ชื่อสินค้า: ${productName} \nจำนวนที่ซื้อ: ${quantity} ชิ้น \nราคารวม: ${totalPrice} บาท \nได้รับส่วนลด: ${discount} \nราคาที่ต้องจ่าย: ${finalPrice} บาท`);
